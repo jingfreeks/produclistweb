34a1a97b381da1af47f6fb6d811134a8d548e5b2
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-09-19 23:36:51 +0800

    udpate baseurl

--- src/config/apiSlice.tsx
+++ src/config/apiSlice.tsx
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500/",
+  baseUrl: "https://hosting-api-xhd4.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers: any) => {
     return headers;
